7561ddd0e23067e1138971eab4f18ad2c95a4454
Ilya Boltnev <ilya@boltnev.com>
2013-05-30 22:19:48 +0400

    refactoring

--- lib/callbackable.rb
+++ lib/callbackable.rb
@@ -1,45 +1,47 @@
+# module Callbackable allows to add callbacks
+# before and after methods  
+# 
+# == Example
+#
+#   require "callbackable"
+#   
+#   class Cat 
+#     include Callbackable
+#   
+#     attr_accessor :name, :colour
+#   
+#     callback :after, :eat, :play
+#     callback :before, :eat, :sleep
+#  
+#     def initialize(name, colour)
+#       @name = name
+#       @colour = colour
+#     end
+#       
+#     def sleep
+#       puts "#{@name} the cat is sleeping"
+#     end
+#   
+#     def eat
+#       puts "#{@name} the cat  is eating"
+#     end
+#   
+#     def play
+#       puts "#{@name} the cat  is playing"
+#     end
+#    
+#     run_callbacks
+#   end
+#  
+#   kitty = Cat.new("Kitty", "black")
+#  
+#   kitty.eat 
+#   #   => Kitty the cat is sleeping
+#   #   => Kitty the cat is eating
+#   #   => Kitty the cat is playing
+
 module Callbackable
   
-  # module Callbackable allows to add callbacks
-  # before and after methods  
-  # 
-  # == Example
-  # require "callbackable"
-  # 
-  # class Cat 
-  #   include Callbackable
-  # 
-  #   attr_accessor :name, :colour
-  # 
-  #   callback :after, :eat, :play
-  #   callback :before, :eat, :sleep
-  #
-  #   def initialize(name, colour)
-  #     @name = name
-  #     @colour = colour
-  #   end
-  #     
-  #   def sleep
-  #     puts "# {@name} the cat is sleeping"
-  #   end
-  # 
-  #   def eat
-  #     puts "# {@name} the cat  is eating"
-  #   end
-  # 
-  #   def play
-  #     puts "# {@name} the cat  is playing"
-  #   end
-  #  
-  #   run_callbacks
-  # end
-  #
-  # kitty = Cat.new("Kitty", "black")
-  #
-  # kitty.eat 
-  # # => Kitty the cat is sleeping
-  # # => Kitty the cat is eating
-  # # => Kitty the cat is playing
   
   class UnknownEventException < Exception; end
   
@@ -57,19 +59,19 @@ module Callbackable
       end
 
       # allows to add a callback to already initialized class
-      # class Cat 
-      #   
-      #   def purr
-      #     #purr
+      # == Example 
+      #   class Cat 
+      #     def purr
+      #       # purr
+      #     end
+      #     
+      #     def sleep
+      #       # sleep
+      #     end
       #   end
-      #   
-      #   def sleep
-      #     #sleep
-      #   end
-      # end
-      #
-      # Cat.add_callback(:after, :purr, :sleep)
-      # Cat.new.purr # purr, then sleep
+      #  
+      #   Cat.add_callback(:after, :purr, :sleep)
+      #   Cat.new.purr #   purr, then sleep
       def self.add_callback(event, method, callback)
         if callbacks_exist? method 
           callback(event, method, callback)
@@ -105,54 +107,61 @@ module Callbackable
         !@@_method_chain[method.to_sym].nil?
       end
       
-      private
-
-      # Register new callback 
-      def self.callback(event, method, callback)
-        reg_callback(event.to_sym, method.to_sym, callback.to_sym)
-      end
+      class << self 
+        private
+        # Register new callback 
+        # See main example
+        def callback(event, method, callback)
+          reg_callback(event.to_sym, method.to_sym, callback.to_sym)
+        end
       
-      def self.del_all_callbacks(method)
-        undef_method method
-        alias_method method, new_method_name(method)
-        @@_method_chain[method] = nil
-      end
+        def del_all_callbacks(method)
+          undef_method method
+          alias_method method, new_method_name(method)
+          @@_method_chain[method] = nil
+        end
       
-      # run all callbacks. 
-      # call it after all methods defined
-      def self.run_callbacks
-        @@_method_chain.each_key do |method|
-          alias_method new_method_name(method), method 
-          undef_method method 
-          
-          define_method method do
-            call_method_chain(method)
+        # run all callbacks. 
+        # call it after all methods defined
+        def run_callbacks
+          @@_method_chain.each_key do |method|
+            run_callback(method)
           end
         end
-      end
-
-      def self.new_method_name(method)
-        "#{method}_#{SALT}"
-      end
-
-      def self.reg_callback(event, method, callback)
-        raise UnknownEventException if (event != :before &&  event != :after)
-        @@_method_chain[method] = [new_method_name(method)] unless @@_method_chain[method]
         
-        if event === :before
-          reg_before_callback(method, callback)
-        elsif event === :after
-          reg_after_callback(method, callback)
-        end        
-      end 
+        def run_callback(method)
+          alias_method new_method_name(method), method 
+          undef_method method 
+        
+          define_method method do
+            call_method_chain(method)
+          end          
+        end
 
-      def self.reg_before_callback(method, callback)
-        @@_method_chain[method].insert \
-           (@@_method_chain[method].index new_method_name(method)), callback
-      end
+        def new_method_name(method)
+          "#{method}_#{SALT}"
+        end
 
-      def self.reg_after_callback(method, callback)
-        @@_method_chain[method].push callback
+        def reg_callback(event, method, callback)
+          raise UnknownEventException if (event != :before &&  event != :after)
+          @@_method_chain[method] = [new_method_name(method)] unless @@_method_chain[method]
+        
+          if event === :before
+            reg_before_callback(method, callback)
+          elsif event === :after
+            reg_after_callback(method, callback)
+          end        
+        end 
+
+        def reg_before_callback(method, callback)
+          @@_method_chain[method].insert \
+             (@@_method_chain[method].index new_method_name(method)), callback
+        end
+
+        def reg_after_callback(method, callback)
+          @@_method_chain[method].push callback
+        end
+      
       end
       
       def call_method_chain(method)
